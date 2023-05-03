349029f7658b48d9c192c55f747babb1f0bff911
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-05-03 16:22:44 -0300

    algoritmo que converter numero binario em decimal

---
+++ converter_para_binario.rb
@@ -0,0 +1,16 @@
+puts 'Digite um numero em decimal'
+numero_decimal = gets.to_i
+
+parte_inteira = (numero_decimal / 2).to_i  ## 255/2 = 127
+resto = (numero_decimal % 2).to_i
+numero_binario = ''
+numero_binario << resto.to_s
+while parte_inteira > 1
+  resto = parte_inteira % 2
+  parte_inteira /= 2
+  numero_binario << resto.to_s
+end
+numero_binario << parte_inteira.to_s
+puts '=============='
+numero_binario = numero_binario.reverse
+puts numero_binario
