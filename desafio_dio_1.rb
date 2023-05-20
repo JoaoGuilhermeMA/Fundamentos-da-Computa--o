fb10a5f5e664140c44c923fe58abf1d9e4492bf4
Joao Guilherme <106107863+JoaoGuilhermeMA@users.noreply.github.com>
2023-05-20 16:18:08 -0300

    Create desafio_dio_1.rb

---
+++ desafio_dio_1.rb
@@ -0,0 +1,11 @@
+puts "Digite seu nome: "
+nome = gets.chomp
+
+puts "Digite seu sobrenome "
+sobrenome = gets.chomp
+
+puts "Digite sua idade "
+idade = gets
+
+puts "nome: #{nome} #{sobrenome}, idade: #{idade}"
+
